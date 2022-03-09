308fdfc362b3eb30a4cef6193a781646fd40e776
uzytkownik <uzytkownik@kursGIT.pl>
2022-03-09 19:41:49 +0100

    add scanner file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main (String[] args){
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Jak  masz na imię?");
+        String name=scanner.nextLine();
+        System.out.println("Jak masz na nazwisko?");
+        String surname= scanner.nextLine();
+        System.out.println("Ile masz lat?");
+        String age= scanner.nextLine();
+        System.out.println("Cześć "+name+
+                " "+surname+" " + "nastolatko!");
+
+
+}
 }
