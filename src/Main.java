16e5bb7ea73fabd16493eac976ffcb53bbbd58a4
uzytkownik <uzytkownik@kursGIT.pl>
2022-03-09 20:02:30 +0100

    add scanner file and age control

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args){
-
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Jak  masz na imię?");
+Scanner scanner = new Scanner(System.in);
+        System.out.println("Jak masz na imię?");
         String name=scanner.nextLine();
         System.out.println("Jak masz na nazwisko?");
-        String surname= scanner.nextLine();
+        String surname=scanner.nextLine();
         System.out.println("Ile masz lat?");
-        String age= scanner.nextLine();
-        System.out.println("Cześć "+name+
-                " "+surname+" " + "nastolatko!");
+        int age= scanner.nextInt();
+        if(age>18) {
+            System.out.println(name+ " " +surname
+                    + ". Możesz wejść");
+        }else {
+          System.out.println(name+ " " +surname
+                  + ". Nie możesz wejść");
+      }
 
 
 }
